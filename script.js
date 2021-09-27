373eb0ed87055c90bd0ee09a9c24e82c8c1ab4e7
ldrljaca <ermin.h@protonmail.com>
2021-09-27 14:02:28 +0200

    finishing the project

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const postsContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 async function getPosts(){
@@ -31,4 +31,43 @@ async function showPosts(){
     });
 }
 
+function showLoading (){
+    loading.classList.add('show');
+
+    setTimeout(() => {
+        loading.classList.remove('show');
+
+        setTimeout(() => {
+            page++;
+            showPosts();
+        },300);
+    }, 1000);
+}
+
+function filterPosts(e){
+    const term = e.target.value.toUpperCase();
+    const posts = document.querySelectorAll('.post');
+
+    posts.forEach(post => {
+        const title = post.querySelector('.post-title').innerText.toUpperCase();
+        const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+        if(title.indexOf(term) > -1 || body.indexOf(term) > -1){
+            post.style.display = 'flex';
+        } else{
+            post.style.display = 'none';
+        }
+    });
+}
+
 showPosts();
+
+window.addEventListener('scroll', () => {
+    const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+
+    if(scrollTop + clientHeight >= scrollHeight - 5){
+        showLoading();
+    }
+});
+
+filter.addEventListener('input', filterPosts);
